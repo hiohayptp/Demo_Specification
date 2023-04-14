a3bf6f88ec92907fbda9b543eaa3e59ac99fc2ad
Phan Thanh Phong <hiohayptp@gmail.com>
2023-04-14 23:54:56 +0700

    create get OTP

---
+++ src/main/java/com/example/randomToken/GetOtpNotificationServiceImpl.java
@@ -0,0 +1,84 @@
+package com.example.randomToken;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import vn.com.mcredit.bnpl.entity.OtpHistory;
+import vn.com.mcredit.bnpl.enums.OtpTypeEnum;
+import vn.com.mcredit.bnpl.repositories.OtpHistoryRepository;
+import vn.com.mcredit.bnpl.repositories.specifications.OtpHistorySpecification;
+import vn.com.mcredit.bnpl.service.GetOtpNotificationService;
+
+import java.sql.*;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+
+public class GetOtpNotificationServiceImpl{
+    private final OtpHistoryRepository otpHistoryRepository;
+
+    private final String URL = "jdbc:postgresql://uat-rds-postgres-common-apps-db.ccjec86kuais.ap-southeast-1.rds.amazonaws.com:5432/ekyc_notification_service";
+    private final String SYSTEM = "onboarding-bnpl";
+    private final String OTP = "otp";
+
+
+    public String getOtp(String phoneNumber) throws SQLException {
+        String user = "devops";
+        String pwd = getPassword();
+        Connection conn = null;
+        String otp = "";
+            try {
+                conn = DriverManager.getConnection(URL + "?user=" + user + "&password=" + pwd);
+                otp = query(conn, phoneNumber);
+            } catch (Exception e) {
+                log.error("OtpPartnerService.getOtpPartner error: " + e.getMessage());
+            } finally {
+                if (!Objects.isNull(conn)) {
+                    conn.close();
+                }
+            }
+            return otp;
+    }
+
+    public String getOtpV2(String phoneNumber, OtpTypeEnum type) {
+        PageRequest pageRequest = PageRequest.of(0, 1, Sort.by("createdDate").descending());
+        List<OtpHistory> otpHistories = otpHistoryRepository.findAll(OtpHistorySpecification.findByPhoneNumberOrTransactionId(phoneNumber, type), pageRequest).getContent();
+        if (!otpHistories.isEmpty() && ObjectUtils.isNotEmpty(otpHistories.get(0))) {
+            return otpHistories.get(0).getCode();
+        }
+        return null;
+    }
+
+    private String query(Connection connection, String phoneNumber) throws SQLException {
+        PreparedStatement preparedStatement = null;
+        String query = "SELECT otp FROM OTP WHERE PHONE_NUMBER = ? AND CREATED_BY = ? ORDER BY CREATED_DATE DESC LIMIT 1";
+        String otp = "";
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, phoneNumber);
+            preparedStatement.setString(2, SYSTEM);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                otp = rs.getString(OTP);
+            }
+        } catch (Exception e) {
+            log.error("OtpPartnerService.query error: " + e.getMessage());
+        } finally {
+            if (!Objects.isNull(preparedStatement)) {
+                preparedStatement.close();
+            }
+        }
+        return otp;
+    }
+
+
+    private String getPassword() {
+        return "9pzMQ96jUaw2";
+    }
+}
---
+++ src/main/java/com/example/randomToken/Run.java
@@ -0,0 +1,15 @@
+package com.example.randomToken;
+
+public class Run {
+    public static void main(String[] args) {
+        String partnerId = "9pay";
+        String clientId = "9pay.bnpl-service";
+        String clientSecret = "NYZjBZXaN5m0jeMKgIEklBZAxatsiRCcp1dizCvS1jmTHiOYzE2ePaQkAIxyoXe/9k9QyYl3EfnUeJnOov48u0H4hByIL1CcUcZsKzi2HGY3QRsxaU6GfGiHCwz6Qjr1banChhBt9fLwuce9w9CN67BzCSj+SD7OyfO0hk9L/dCAvNHgkb7D1ikVnQl5fHuYme5baMtAgTBYaxo2It3cX579WtnTdqZHF/yqUpH08FnaF32MHWDiheTDyZeR3Br0pkVy5YFIoVybvMMEpTqWs0FMhII/DS+UT2PgvzqQTcF4fD/td01MIWKyvRsEWxIKhHFNL3iJZcMISDtoxUT1uz/ouw+faZ4gz8R+zwsNCWmeE9P2vT9nbj3jJgWpNDj0YbSvAQ5rSKCWTSBv3oKwnNyIx5XDiPLqZ5sQik0acJp4t3i0m1aQ2NTL0Wu7+FSqDVzcREutAXzk6czcl0aeKXM8yCcsO8rDDDi4ln1ubKZSkHHfY2tpHkMuhU128/M+4aeq9DC+IVPWCRvQYY4O5evrj9zTAE5ElZi3dUYmXc0SdI9lDv9nPXj7mfLBuofstThUQxrjFs+hMbCFLtO2vLxiQWVD0PBZNMT+PxVCPNmQOzw1W5plBjm4/NY5v+kQqjsjqQgxzaSR+W8mdfHfLoGFV4C5QDQf+/2Q7h/EK0s=";
+        String deviceId = "800ah";
+        String accountId = "7786969abc";
+        String phoneNumber = "0335430008";
+        String identityCard = "080060009455";
+
+
+    }
+}
