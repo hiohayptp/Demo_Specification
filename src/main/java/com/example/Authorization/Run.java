82790bb14489d4c397fc786fafa7d5cee223d031
Phan Thanh Phong <hiohayptp@gmail.com>
2023-04-26 11:15:39 +0700

    update

--- src/main/java/com/example/Authorization/Run.java
+++ src/main/java/com/example/Authorization/Run.java
@@ -1,13 +1,9 @@
 package com.example.Authorization;
 
-import com.example.Authorization.IAuthService;
 import org.springframework.boot.SpringApplication;
 
 public class Run {
     public static void main(String[] args) {
-//        @Authorization
-//        IAuthService authService;
-//        String token = authService.getWebView();
         SpringApplication.run(Run.class);
     }
 }
